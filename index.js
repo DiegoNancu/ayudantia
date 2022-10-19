a44a96d5344c6f3a6a80e79b51c43bf7fe434373
DiegoNancu <d_nancupillan@hotmail.com>
2022-10-19 00:51:55 -0300

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const mongoose = requiere('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('Server started on port 3000');
 })
