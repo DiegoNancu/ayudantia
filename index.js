ab4e508095e5622a6784f1a6959290a8978288af
DiegoNancu <d_nancupillan@hotmail.com>
2022-10-19 00:29:04 -0300

    firts commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = requiere('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.listen(3000,() => {
+    console.log('Server started on port 3000');
+})
